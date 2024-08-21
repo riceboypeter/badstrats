5ad3112215ae3ccebed64b882a6d73a17eb7574b
Peter Qiu <77300475+riceboypeter@users.noreply.github.com>
2024-08-21 13:08:17 -0700

    brainstorming (￣┰￣*)

--- badstrats/src/App.js
+++ badstrats/src/App.js
@@ -1,13 +1,58 @@
 import logo from './logo.svg';
 import './App.css';
 
+/*
+attachment rules & weight:
+20% shhh... (only silencers)
+15% one tap (muzzle break & single shot)
+15% raw dog (no attachments)
+20% squint (irons only)
+30% no rules
+
+gameplay rules:
+never left CS (hipfire only)
+eco round (pistol only)
+red light green light (no moving; opens modal)
+short kings (crouch/crawl only)
+mlg (360 before each time you shoot)
+one bullet (start of round, make sure there's only one the chamber; you can only play normally after you get the kill)
+upstairs neighbours (make as much noise as possible throughout the round)
+no gadgets
+no rules
+
+strat:
+both:
+follow the leader
+vertical only
+make them scared: only swing attackers that you hard ping on a camera/drone, otherwise only hold angles
+prefire everything: every single angle you clear when you're near an attacker must be prefired
+no rules
+
+defense:
+open concept
+hacker spawnpeek 
+flip the tables: kill the attackers from the outside only
+spawnpeek!
+the one piece: when you find defuser, stand on it and defend it with your life
+blind: break all cams (including outside ones)
+onlyfeet: make as many one way foot holes as physically possible (no reinforcing)
+
+attack:
+rush B
+never stop moving
+starting from the furthest point, go through as many walls as possible in a straight line to get to site
+clay pigeon: throw out your drone and attempt to shoot it out of the air; only once you finish your aim training can you enter the building
+clear out the building: drone ahead 2 rooms for each 1 room you go through
+
+*/
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ayo i'm a real <code>glizzy gobbler</code> and that's on god
         </p>
         <a
           className="App-link"
